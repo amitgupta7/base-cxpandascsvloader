96d57acacab3ebf763315a4d77f6abe0099a5b3e
amitgupta7 <amit864@gmail.com>
2024-12-19 16:04:00 +0530

    testing github actions cicd publishing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cxcsvtopandas",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     author="Amit Gupta",
     author_email="amitgupta@example.com",
