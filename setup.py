c5fc86f6fab63d13bc2a43a5e123767721edcb25
amitgupta7 <amit864@gmail.com>
2025-01-07 11:11:44 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cxcsvtopandas",
-    version="0.1.3",
+    version="0.1.4",
     packages=find_packages(),
     author="Amit Gupta",
     author_email="amit864@gmail.com",
